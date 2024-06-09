8c6bf8d0cd99f6e30e8903dc1a82c5860d019731
Maruti <mbkolavale@gmail.com>
2024-06-09 16:04:33 +0530

    update BuzzPOM page 09-june-2024

--- E-commerce/src/test/java/PageObject/BuzzPOM.java
+++ E-commerce/src/test/java/PageObject/BuzzPOM.java
@@ -1,14 +1,17 @@
 package PageObject;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class BuzzPOM {
-
 	
 	public BuzzPOM(WebDriver driver)
 	{
 		PageFactory.initElements(driver,this);
 	}
 	//
+	@FindBy(xpath ="//button[@type='submit']") public WebElement postButton;
+	@FindBy(xpath ="//button[text()=' Share Photos']") public  WebElement sharePhoto;
 }
